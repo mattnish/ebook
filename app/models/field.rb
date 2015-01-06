649f5ffccaec38e468e1d4110f00f73e652a4341
Matt Nish <matt.nish@gmail.com>
2015-01-05 18:36:21 -0800

    Removed model 'Field' so that we'll just use 'FieldGroup' for everything

--- app/models/field.rb
+++
@@ -1,7 +0,0 @@
-class Field < ActiveRecord::Base
-  belongs_to :page, polymorphic: true
-  has_one :text_field, as: :field
-  has_one :image_field, as: :field
-  has_one :wysiwyg_field, as: :field
-
-end
